17bc4ba43c6db5e1e3b6cd658ef5e92f298c389d
mcsasha <areumjinne@gmail.com>
2021-06-03 00:21:43 +0900

    practicing java

--- first/src/day01/CharTest.java
+++ first/src/day01/CharTest.java
@@ -3,22 +3,23 @@ package day01;
 public class CharTest {
 	public static void main(String[] args) {
 		
-		//대소문자
-		char c1 = 'A';
+		char c1 = 'A';		// 대문자 A = 65
 		int c2 = c1 + 32;
-		System.out.println(c2 + " "+ (char)c2);
+		
+		System.out.println(c2 + " " + (char)c2);	// 소문자 a = 97
 		
 		char c3 = '\uac00';
 		char c4 = 0xac00;
 		char c5 = '가';
 		char c6 = 44032;
+		
 		System.out.println(c3);
 		System.out.println(c4);
 		System.out.println(c5);
 		System.out.println(c6);
 		
-		//문자열
-		String s = "안녕\"\n\t하세요\\";
+		String s = "\"\thello\"\neveryone\\";
+		
 		System.out.println(s);
 	}
 
