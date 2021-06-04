2367c387f326574d98ec0264f6ad7a17d3427eb5
admin <areumjinne@gmail.com>
2021-06-04 17:50:59 +0900

    practicing java

--- 7일차/src/co/yedam/app/file/FileUtil.java
+++ 7일차/src/co/yedam/app/file/FileUtil.java
@@ -3,13 +3,13 @@ package co.yedam.app.file;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class FileCopyTest {
-	public static void main(String[] args) {
+public class FileUtil {
+	public static void copy (String src, String des) {
 		
 		// 파일 복사
 		try {
-			FileInputStream fis = new FileInputStream("d:/temp/img.jpg");
-			FileOutputStream fos = new FileOutputStream("d:/temp/img2.jpg");
+			FileInputStream fis = new FileInputStream(src);
+			FileOutputStream fos = new FileOutputStream(des);
 			byte[] b = new byte[100];
 			while(true) {
 				int cnt = fis.read(b);
